c6a5f58b1c547da10bbcffd507ac47a2f6af85bb
Nathan Petryk <npetryk@rmn.com>
2018-01-05 15:37:12 -0600

    Version 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rmn/version-qualified "1.0.1-SNAPSHOT"
+(defproject com.rmn/version-qualified "1.0.1"
   :description "A library for generating version-specific code"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
